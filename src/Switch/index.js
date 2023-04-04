fb47483d7554ad91ff4d901a2eb7b0f55c57e01e
joan lovelle alfeche <jlovellealfeche@gmail.com>
2023-04-04 15:13:35 +0200

    completing switch\index.js

--- src/Switch/index.js
+++ src/Switch/index.js
@@ -1,13 +1,19 @@
 import "./Styles.css";
 import { useTheme } from "../ThemeContext";
 
+/* Step 3 switch component can then be connected to the toggleTheme function returned useTheme.
+ Put toggleTheme next to the const {theme, ____here____} 
+ -don't forget the onChange
+*/
+
 const Switch = () => {
-  const { theme } = useTheme();
+  const { theme, toggleTheme } = useTheme();
   return (
     <label className="switch">
       <input
         type="checkbox"
         checked={theme === "light"}
+        onChange={toggleTheme}
       />
       <span className="slider round" />
     </label>
--- src/ThemeContext.js
+++ src/ThemeContext.js
@@ -31,3 +31,5 @@ export const useTheme = () =>useContext(ThemeContext);
 
 
 
+
+
